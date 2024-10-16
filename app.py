df1261f6081fb4cf6b60d1150924406b23748c4f
mukundgupta <mukundgupta2121@gmail.com>
2024-10-16 21:13:09 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import main
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    
+    return render_template("index.html")
+
+@app.route('/page')
+def map_page():
+    c,directions = main.main()
+    
+    return render_template("page.html",solution=c,directions=directions)
+
+@app.route('/donor_form')
+def donor_form():
+    return render_template('donor_form.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
